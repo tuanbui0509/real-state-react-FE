63fcf164ec61298a34d0e0331b4b115c0d0a0a79
tuanbui0509 <tuanbui0509@gmail.com>
2021-06-22 21:03:00 +0700

    create Navbar

---
+++ src/data/MenuData.js
@@ -0,0 +1,15 @@
+export const MenuData = [
+    {
+        title: 'About',
+        link:'/about'
+    },
+    {
+        title: 'Home',
+        link:'/homes'
+    },
+    {
+        title: 'Rentals',
+        link:'/rentals'
+    },
+
+]
